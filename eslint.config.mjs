c26dfb2b49a8b36f2ef97208c81013278b3e88a5
Victor Yuan <victor.2wy@gmail.com>
2025-08-17 22:00:06 -0700

    update eslint syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,12 @@ import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: [js.configs.recommended],
+  },
   globalIgnores(["webpack.config.js"]),
 ]);
