fec6c2652fd1e61d807aaaa858109fc454c2460a
Victor Yuan <victor.2wy@gmail.com>
2025-08-17 21:59:54 -0700

    click to reveal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+
+function makeDropDownItemsHidden() {
+  const dropdownDiv = document.querySelector(".dropdown");
+  const itemsList = dropdownDiv.querySelector(".dropdown-items-list");
+  itemsList.classList.remove("visible");
+  itemsList.classList.add("hidden");
+}
+
+function addDropDownListener() {
+  const dropdownDiv = document.querySelector(".dropdown");
+  const title = dropdownDiv.querySelector(".dropdown-title");
+  const itemsList = dropdownDiv.querySelector(".dropdown-items-list");
+
+  title.addEventListener("click", () => {
+      itemsList.style.display = 'block';
+    
+  });
+}
+
+window.addEventListener("click", function (e) {
+  const dropdownDiv = document.querySelector(".dropdown");
+  const itemsList = dropdownDiv.querySelector(".dropdown-items-list");
+
+  if (itemsList.contains(e.target)) {
+    // Clicked in box
+  } else {
+    // // Clicked outside the box
+    // itemsList.style.display = 'none';
+    // itemsList.classList.add("hidden");
+  }
+});
+
+makeDropDownItemsHidden();
+addDropDownListener();
