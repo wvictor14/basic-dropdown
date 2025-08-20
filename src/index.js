94f9c31cf492d461149c165e6e9580412286801f
Victor Yuan <victor.2wy@gmail.com>
2025-08-19 20:28:56 -0700

    make it apply to multiple dropdowns

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import "./styles.css";
 
 function addDropDownListener() {
-  const dropdownDiv = document.querySelector(".dropdown");
-  if (!dropdownDiv) return;
+  const dropdowns = document.querySelectorAll(".dropdown");
+  if (!dropdowns) return;
 
-  const title = dropdownDiv.querySelector(".dropdown-title");
-  const itemsList = dropdownDiv.querySelector(".dropdown-items-list");
-  if (!title || !itemsList) return;
-  
-  // initialize the items with the hidden class
-  itemsList.classList.add("hidden");
-  itemsList.classList.remove("visible");
+  dropdowns.forEach(dropdown => {
 
-  // add event listener that toggles the class on/off
-  const handleClick = function () {
-    itemsList.classList.toggle("hidden");
-    itemsList.classList.toggle("visible");
-  }
+    const title = dropdown.querySelector(".dropdown-title");
+    const itemsList = dropdown.querySelector(".dropdown-items-list");
+    if (!title || !itemsList) return;
 
-  title.addEventListener("click", handleClick);
+    // initialize the items with the hidden class
+    itemsList.classList.add("hidden");
+    itemsList.classList.remove("visible");
+
+    // add event listener that toggles the class on/off
+    const handleClick = function () {
+      itemsList.classList.toggle("hidden");
+      itemsList.classList.toggle("visible");
+    }
+
+    title.addEventListener("click", handleClick);
+  })
 }
 
 addDropDownListener();
